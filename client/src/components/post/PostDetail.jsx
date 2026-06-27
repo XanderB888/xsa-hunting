b26b44645ffd399874af43a15fd3b7bc1549b661
Xander <xander.cage.botha@gmail.com>
2026-06-27 22:17:59 +0100

    Add owner-only delete button to PostDetail

--- client/src/components/post/PostDetail.jsx
+++ client/src/components/post/PostDetail.jsx
@@ -3,10 +3,17 @@ import { mockPosts } from '../../mockData.js';
 import ShotPlacementPanel from './ShotPlacementPanel.jsx';
 import FirearmInfo from './FirearmInfo.jsx';
 import CommentList from '../comments/CommentList.jsx';
+import { useAuth } from '../../context/AuthContext.jsx';
 
 function PostDetail() {
   const { id } = useParams();
   const post = mockPosts.find((p) => p.id === Number(id));
+  const { user } = useAuth();
+  const isOwner = user && user.username === post.username;
+
+  const handleDelete = () => {
+    console.log('Delete post', post.id);
+  }; // This is for testing, real Deleteing will be implemented later
 
   if (!post) {
     return <div>Post not found</div>;
@@ -24,6 +31,9 @@ function PostDetail() {
       <ShotPlacementPanel shot={post.shotPlacement} />
       <FirearmInfo firearm={post.firearm} />
       <CommentList comments={post.comments} />
+      {isOwner && (
+        <button onClick={handleDelete}>Delete this post</button>
+      )}
     </div>
   );
 }
