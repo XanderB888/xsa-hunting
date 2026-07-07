3aaa6c0adc92eeb27df734e54a8e63758006160f
Xander <xander.cage.botha@gmail.com>
2026-07-07 23:48:49 +0100

    Add connection timeout

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  connectionTimeoutMillis: 10000,   // ← fail after 10s instead of hanging forever
 });
 
 module.exports = pool;
