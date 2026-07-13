5ee3887aef5d65110c58d773dd72ebc2cd5eb1de
Xander <xander.cage.botha@gmail.com>
2026-07-13 20:18:56 +0100

    fix JPG to jpg for 404 error

--- client/src/gallery/gallery.js
+++ client/src/gallery/gallery.js
@@ -1,26 +1,26 @@
 export const SHOT_IMAGES = {
   Impala: {
-    "Side L": "/shots/impala-left.JPG",
+    "Side L": "/shots/impala-left.jpg",
     "Side R": "/shots/impala-right.jpg",
   },
   Kudu: {
-    "Side L": "/shots/kudu-left.JPG",
+    "Side L": "/shots/kudu-left.jpg",
     "Side R": "/shots/kudu-right.jpg",
   },
   Warthog: {
     "Side L": "/shots/warthog-left.jpg",
-    "Side R": "/shots/warthog-right.JPG",
+    "Side R": "/shots/warthog-right.jpg",
   },
   BushPig: {
-    "Side L": "/shots/bushpig-left.JPG",
-    "Side R": "/shots/bushpig-right.JPG",
+    "Side L": "/shots/bushpig-left.jpg",
+    "Side R": "/shots/bushpig-right.jpg",
   },
   Zebra: {
     "Side L": "/shots/zebra-left.jpg",
-    "Side R": "/shots/zebra-right.JPG",
+    "Side R": "/shots/zebra-right.jpg",
   },
   BlueWildeBeest: {
-    "Side L": "/shots/bwb-left.JPG",
+    "Side L": "/shots/bwb-left.jpg",
     "Side R": "/shots/bwb-right.jpg",
   },
 };
